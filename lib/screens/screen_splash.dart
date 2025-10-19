475dc364ad3a6d0bad610eb5d8ef16721c61507d
Ryan Nardelli <ryannardelli@gmail.com>
2025-10-19 17:38:01 -0300

    enviando mudança de cor na tela de splash screen

--- lib/screens/screen_splash.dart
+++ lib/screens/screen_splash.dart
@@ -21,12 +21,30 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xFFD90404),
-      body: Center(
-        child: Image.asset('assets/logo-app.png', height: 150),
+Widget build(BuildContext context) {
+  return Scaffold(
+    // removemos o backgroundColor
+    body: Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            Color(0xFFD90404), // vermelho intenso
+            Color(0xFFFF5E5E), // vermelho mais claro
+          ],
+        ),
       ),
-    );
-  }
+      child: Center(
+        child: Image.asset(
+          'assets/logo-app.png',
+          height: 150,
+        ),
+      ),
+    ),
+  );
+}
+
 }
